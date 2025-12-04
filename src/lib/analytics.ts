c976020ab4403ce2746298953ae57d33279d7aa3
Ramsés Roy Adorno <76007299+ramsesroy@users.noreply.github.com>
2025-12-03 21:11:10 -0300

    Corregir error de tipo en analytics.ts

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -30,7 +30,7 @@ export const initGA = (measurementId: string) => {
   window.gtag = function () {
     window.dataLayer?.push(arguments);
   };
-  window.gtag("js", new Date());
+  window.gtag("js", new Date().toISOString());
   window.gtag("config", measurementId, {
     page_path: window.location.pathname,
   });
